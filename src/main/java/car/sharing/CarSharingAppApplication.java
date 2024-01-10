d0ecf49d864b9a4bc4b9a32a43357c92391d1767
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-01-10 21:39:17 +0200

    Updated infrastructure

---
+++ src/main/java/car/sharing/CarSharingAppApplication.java
@@ -0,0 +1,13 @@
+package car.sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/car/sharing/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package car.sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
