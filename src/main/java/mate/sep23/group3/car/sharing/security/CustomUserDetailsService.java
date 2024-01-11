e9c0c4361120bc4f239d1c34ef214a342a9dca4f
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-11 20:59:39 +0100

    added CustomUserDetailsService

---
+++ src/main/java/mate/sep23/group3/car/sharing/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package mate.sep23.group3.car.sharing.security;
+
+import lombok.RequiredArgsConstructor;
+import mate.sep23.group3.car.sharing.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private static final String CAN_NOT_FIND_USER_BY_EMAIL = "Can't find user by email";
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(CAN_NOT_FIND_USER_BY_EMAIL));
+    }
+}
