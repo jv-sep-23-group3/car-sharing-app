4d3e82c8af9d1d19d0c3b478023e1822bcbf31ca
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-11 16:01:20 +0100

    commented on the test

--- src/test/java/mate/sep23/group3/car/sharing/CarSharingAppApplicationTests.java
+++ src/test/java/mate/sep23/group3/car/sharing/CarSharingAppApplicationTests.java
@@ -3,11 +3,11 @@ package mate.sep23.group3.car.sharing;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CarSharingAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class CarSharingAppApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
