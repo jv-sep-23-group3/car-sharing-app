cdb7d770482ae97b876201390d1d07b5e73dfbb9
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-01-11 20:57:06 +0200

    Added create customer method in payment service

---
+++ src/main/java/mate/sep23/group3/car/sharing/service/PaymentService.java
@@ -0,0 +1,8 @@
+package mate.sep23.group3.car.sharing.service;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+
+public interface PaymentService {
+    public void createCustomer(String email, String firstName, String lastName) throws StripeException;
+}
---
+++ src/main/java/mate/sep23/group3/car/sharing/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,33 @@
+package mate.sep23.group3.car.sharing.service.impl;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import com.stripe.param.CustomerCreateParams;
+import jakarta.annotation.PostConstruct;
+import mate.sep23.group3.car.sharing.service.PaymentService;
+import org.springframework.beans.factory.annotation.Value;
+
+public class PaymentServiceImpl implements PaymentService {
+    @Value("${stripe.api.key}")
+    private String apiKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = apiKey;
+    }
+
+    @Override
+    public void createCustomer(String email, String firstName, String lastName) {
+        CustomerCreateParams createParams = CustomerCreateParams.builder()
+                .setEmail(email)
+                .setName(String.format("%s %s", firstName, lastName))
+                .build();
+
+        try {
+            Customer.create(createParams);
+        } catch (StripeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
