def7fa5ab763d52dc5b750ab181f38eb875d7978
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-11 19:06:30 +0100

    created password matches validator

---
+++ src/main/java/mate/sep23/group3/car/sharing/validation/FieldMatch.java
@@ -0,0 +1,20 @@
+package mate.sep23.group3.car.sharing.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = PasswordMatchesValidator.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FieldMatch {
+    String message() default "Repeated password doesn't match";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/mate/sep23/group3/car/sharing/validation/PasswordMatchesValidator.java
@@ -0,0 +1,17 @@
+package mate.sep23.group3.car.sharing.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import mate.sep23.group3.car.sharing.dto.user.UserRegistrationRequestDto;
+
+import java.util.Objects;
+
+public class PasswordMatchesValidator implements ConstraintValidator<FieldMatch,
+        UserRegistrationRequestDto> {
+    @Override
+    public boolean isValid(UserRegistrationRequestDto user,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        return user.getPassword() != null
+                && Objects.equals(user.getPassword(), user.getRepeatPassword());
+    }
+}
