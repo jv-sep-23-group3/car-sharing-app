8d8bddabd3ccff1f974d6e3f06620d041c7664c7
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-01-11 15:23:51 +0200

    Updated payment model

--- src/main/java/mate/sep23/group3/car/sharing/model/Payment.java
+++ src/main/java/mate/sep23/group3/car/sharing/model/Payment.java
@@ -3,27 +3,31 @@ package mate.sep23.group3.car.sharing.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 
 @Data
 @Entity
 @Accessors(chain = true)
+@Table(name = "payments")
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     private Status status;
     @Column(nullable = false)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     private Type type;
     @Column(nullable = false)
-    private Object session; //TO CHANGE
-    @Column(nullable = false, unique = true)
+    private String session;
+    @Column(name = "session_id", nullable = false, unique = true)
     private String sessionId;
     @Column(nullable = false)
     private BigDecimal amount;
-
 
     public enum Status {
         PENDING,
