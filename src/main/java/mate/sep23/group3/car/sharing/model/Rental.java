c13e0c6e8cba6a58480ea4af5906321a36eb2515
Danil Ponomarenko <akame.dante13@gmail.com>
2024-01-11 13:08:53 +0200

    Add rental entity

--- src/main/java/mate/sep23/group3/car/sharing/model/Rental.java
+++ src/main/java/mate/sep23/group3/car/sharing/model/Rental.java
@@ -1,4 +1,48 @@
 package mate.sep23.group3.car.sharing.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE rentals SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted=false")
+@Table(name = "rentals")
 public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @JoinColumn(name = "car_id", nullable = false)
+    private Car carId;
+
+    @OneToOne
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    @Column(nullable = false)
+    private LocalDate rentalDate;
+    @Column(nullable = false)
+    private LocalDate returnDate;
+    private LocalDate actualReturnDate;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
