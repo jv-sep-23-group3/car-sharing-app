f0c307852c1fd5d3b18ee77441af77882dd25954
Dima Semeniuk <semeniuk.dima@ukr.net>
2024-01-12 16:40:17 +0200

    hide test method

--- src/test/java/mate/sep23/group3/car/sharing/CarSharingAppApplicationTests.java
+++ src/test/java/mate/sep23/group3/car/sharing/CarSharingAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CarSharingAppApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
