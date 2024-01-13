72300aa4766a904d7ffe6beb4ce7aa722709f34e
Dima Semeniuk <semeniuk.dima@ukr.net>
2024-01-13 17:31:57 +0200

    mede change in car structure

--- src/main/java/mate/sep23/group3/car/sharing/dto/car/CarResponseDto.java
+++ src/main/java/mate/sep23/group3/car/sharing/dto/car/CarResponseDto.java
@@ -7,8 +7,8 @@ import mate.sep23.group3.car.sharing.model.Car;
 @Data
 public class CarResponseDto {
     private Long id;
-    private String model;
     private String brand;
+    private String model;
     private Car.Type type;
     private int inventory;
     private BigDecimal dailyFee;
--- src/main/java/mate/sep23/group3/car/sharing/scheduled/OverdueRentalsNotification.java
+++ src/main/java/mate/sep23/group3/car/sharing/scheduled/OverdueRentalsNotification.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import mate.sep23.group3.car.sharing.model.Rental;
 import mate.sep23.group3.car.sharing.repository.RentalRepository;
 import mate.sep23.group3.car.sharing.telegram.TelegramBot;
+import org.checkerframework.checker.units.qual.C;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,6 +18,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OverdueRentalsNotification {
     private static final int THREE_HOURS = 3;
+    private static final String INDENTATION = " ";
+    private static final String COMA = ",";
+    private static final String DAILY_FEE = "daily fee: ";
     @Value("${admin.chat.id}")
     private Long adminChatId;
     private final TelegramBot telegramBot;
@@ -29,7 +33,10 @@ public class OverdueRentalsNotification {
         for (Rental rental : rentals) {
             if (ChronoUnit.HOURS.between(LocalDateTime.now(),
                     rental.getReturnDate()) < THREE_HOURS) {
-                String message = rental.getCar() + System.lineSeparator()
+                String message = rental.getCar().getBrand() + INDENTATION
+                        + rental.getCar().getModel() + COMA + INDENTATION
+                        + DAILY_FEE + rental.getCar().getDailyFee()
+                        + System.lineSeparator()
                         + "Returned date :" + rental.getReturnDate();
 
                 messageForManagers.append(rental.getUser().getEmail())
