27f0ca4b60bf6f3de4a58753ebb7c1b1471ca723
Dima Semeniuk <semeniuk.dima@ukr.net>
2024-01-15 16:37:11 +0200

    add dependencies for testcontainers and tests structure with CustomMySqlContainer

---
+++ src/test/resources/database/cars/add-car-to-cars-table.sql
@@ -0,0 +1,3 @@
+insert into cars (id, brand, model, type, inventory) values (1, 'BMW', 'X5', 'SUV', 120);
+insert into cars (id, title, author, isbn, price) values (2, 'Mercedes', 'E212', 'SEDAN', 70);
+insert into cars (id, title, author, isbn, price) values (3, 'Audi', 'A6', 'UNIVERSAL', 80);
