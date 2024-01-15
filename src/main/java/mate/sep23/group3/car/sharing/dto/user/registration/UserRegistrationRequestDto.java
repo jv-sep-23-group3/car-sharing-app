76721cf153e31d834dde3e72579d8a043f4f8f7b
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-15 16:51:12 +0100

    changed @FieldMatch to @FieldMatchRegistration

--- src/main/java/mate/sep23/group3/car/sharing/dto/user/registration/UserRegistrationRequestDto.java
+++ src/main/java/mate/sep23/group3/car/sharing/dto/user/registration/UserRegistrationRequestDto.java
@@ -1,13 +1,13 @@
-package mate.sep23.group3.car.sharing.dto.user;
+package mate.sep23.group3.car.sharing.dto.user.registration;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import mate.sep23.group3.car.sharing.validation.FieldMatch;
+import mate.sep23.group3.car.sharing.validation.FieldMatchRegistration;
 import org.hibernate.validator.constraints.Length;
 
 @Data
-@FieldMatch
+@FieldMatchRegistration
 public class UserRegistrationRequestDto {
     private static final String NOT_BE_NULL = "Can't be null";
     private static final String FROM_8_TO_24_CHARACTERS = "Must contain 8 to 24 characters long";
