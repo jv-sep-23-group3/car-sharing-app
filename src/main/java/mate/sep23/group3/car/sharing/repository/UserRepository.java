ab7a355ea6434fd98a37796fd9d08eb9ceb56cb7
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-15 16:52:02 +0100

    added new methods to service, mapper, repository

--- src/main/java/mate/sep23/group3/car/sharing/repository/UserRepository.java
+++ src/main/java/mate/sep23/group3/car/sharing/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package mate.sep23.group3.car.sharing.repository;
 
 import java.util.Optional;
+
+import jakarta.persistence.Entity;
 import mate.sep23.group3.car.sharing.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +14,10 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
     Optional<User> findByEmail(@Param("email") String email);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.id = :id")
+    Optional<User> findRolesInUser(@Param("id") Long id);
+
+    @EntityGraph(attributePaths = "roles")
+    Optional<User> findById(Long id);
 }
