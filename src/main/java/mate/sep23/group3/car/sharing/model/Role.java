04b753aaebc311d7a308a5a6a4288c109c7b9926
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-15 16:50:38 +0100

    added methods to the RoleName enumeration

--- src/main/java/mate/sep23/group3/car/sharing/model/Role.java
+++ src/main/java/mate/sep23/group3/car/sharing/model/Role.java
@@ -6,10 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.Data;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 import org.springframework.security.core.GrantedAuthority;
+
 
 @Entity
 @Data
@@ -28,7 +33,28 @@ public class Role implements GrantedAuthority {
         return "ROLE_" + roleName.name();
     }
 
+
     public enum RoleName {
-        MANAGER, CUSTOMER, ADMIN
+        MANAGER(0), CUSTOMER(1), ADMIN(2);
+
+        private final int index;
+
+        RoleName(int index) {
+            this.index = index;
+        }
+
+        public Integer getIndex() {
+            return index;
+        }
+
+        public static RoleName getByIndex(int index) {
+            for (RoleName roleName : RoleName.values()) {
+                if (roleName.getIndex() == index) {
+                    return roleName;
+                }
+            }
+            throw new IllegalArgumentException("No enum constant with index: " + index);
+        }
+
     }
 }
