2b839ce7facd38d1418d141196f2162c7f954cbc
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-16 11:10:05 +0100

    fixed dto (correct return roles)

--- src/main/java/mate/sep23/group3/car/sharing/dto/user/role/UserWithRoleRequestDto.java
+++ src/main/java/mate/sep23/group3/car/sharing/dto/user/role/UserWithRoleRequestDto.java
@@ -2,10 +2,9 @@ package mate.sep23.group3.car.sharing.dto.user.role;
 
 import java.util.Set;
 import lombok.Data;
-import mate.sep23.group3.car.sharing.model.Role;
 
 @Data
 public class UserWithRoleRequestDto {
     private Long id;
-    private Set<Role> roles;
+    private Set<Long> roles;
 }
